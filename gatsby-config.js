86ddf59a48aecd698048b208317f1d15ea4e9ada
Mark Foster <foster154@gmail.com>
2019-10-05 08:10:37 -0600

    add Google Analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-102370279-1`,
       },
     },
     {
