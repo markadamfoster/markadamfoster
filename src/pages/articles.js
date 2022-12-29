e371ea3beec417a7dffd3014aad8b67f7a3439cb
Mark Foster <foster154@gmail.com>
2022-12-29 11:18:18 -0600

    add SEO component to main pages

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,40 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import { Helmet } from 'react-helmet'
+import ArticlesPage from 'components/Articles/ArticlesPage'
 
-import DefaultLayout from '../layouts/DefaultLayout'
-import ArticlesPageContent from 'components/Articles/ArticlesPageContent'
-
-ArticlesPage.propTypes = {
-  data: PropTypes.object,
-}
-
-function ArticlesPage({ data }) {
-  const siteTitle = data.site.siteMetadata.title
-  const siteDescription = data.site.siteMetadata.description
-
-  return (
-    <DefaultLayout>
-      <Helmet
-        htmlAttributes={{ lang: 'en' }}
-        meta={[{ name: 'description', content: siteDescription }]}
-        title={`Articles | ${siteTitle}`}
-      />
-      <ArticlesPageContent />
-    </DefaultLayout>
-  )
-}
-
+export { Head } from 'components/Articles/ArticlesPage'
 export default ArticlesPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-  }
-`
